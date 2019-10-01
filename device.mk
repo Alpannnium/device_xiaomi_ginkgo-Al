3d64b9932711e3082859ffe4634484dbd1f6af2f
Jake Weinstein <jake@aospa.co>
2019-10-01 04:46:59 +0200

    ginkgo: Use speed tuning for performance critical applications
    The default ART setting is quicken: run DEX code verification and
    optimize some DEX instructions to get better interpreter performance.
    
    This commit switches performance critical applications to speed:
    run DEX code verification and AOT-compile all methods.
    
    PRODUCT_DEXPREOPT_SPEED_APPS (New in Android O)
    List of applications that have been identified as core to the products
    and which are desirable to compile with the speed compiler filter.  For
    example, persistent apps such as SystemUI get a chance to use
    profile-guided compilation only at the next reboot, so it may be better
    for the product to have these apps always AOT-compiled.
    
    Change-Id: Ib2dc9a55dfb332ba3fb84a13544d564a63f37ad2
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: Whot1966 <Whot1966@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -497,3 +497,7 @@ PRODUCT_BOOT_JARS += \
 # Dex2oat
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    Settings \
+    SystemUI
