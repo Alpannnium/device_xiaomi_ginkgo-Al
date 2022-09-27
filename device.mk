640abdadac42d6a7284b4a414762f40e1dc3732d
DarkJoker360 <simoespo159@gmail.com>
2022-09-27 00:23:26 -0700

    ginkgo: Drop MiuiCamera support
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- device.mk
+++ device.mk
@@ -14,7 +14,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
-    GinkgoMiuiCamera \
     GinkgoNoCutoutOverlay \
     NotchBarKiller \
     WillowFrameworkRes
@@ -91,9 +90,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
-# ANXCamera
-$(call inherit-product, vendor/xiaomi/miuicamera/config.mk)
-
+# Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
@@ -228,10 +225,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
-
-# Keyhandler
-PRODUCT_PACKAGES += \
-    KeyHandler
 
 # Keymaster
 PRODUCT_PACKAGES += \
