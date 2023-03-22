35541894ce38d831f9e5ac1cbb641646ffd440e6
Dyneteve <sound0020@gmail.com>
2023-03-22 08:09:12 +0545

    ginkgo: Enable AOSP surfaceflinger
    * AOSP SurfaceFlinger is smoother than CAF

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -166,6 +166,9 @@ SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 # Treble
 BOARD_VNDK_VERSION := current
 
+# SurfaceFlinger
+TARGET_USE_AOSP_SURFACEFLINGER := true
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
