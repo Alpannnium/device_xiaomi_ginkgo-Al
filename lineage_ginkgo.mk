b0b5b1ec165e5cc3e6b6e12b230e2995d0a8da24
neophyte <neophyteplayground@gmail.com>
2026-01-04 22:24:49 +0700

    ginkgo: Adapt to @LineageOS-T
    Signed-off-by: neophyte <neophyteplayground@gmail.com>

--- lineage_ginkgo.mk
+++ lineage_ginkgo.mk
@@ -23,13 +23,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from ginkgo device
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
-# Inherit some common ArrowOS stuff
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_PIXEL_CHARGER := true
-$(call inherit-product, vendor/superior/config/common.mk)
+# Inherit some common LineageOS stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Maintainer name
+DEVICE_MAINTAINER := neophyte
+DEVICE_MAINTAINER_LINK := https://t.me/k4ngcaribug
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
 
 # Device identifier
-PRODUCT_NAME := superior_ginkgo
+PRODUCT_NAME := lineage_ginkgo
 PRODUCT_DEVICE := ginkgo
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 8/8T
