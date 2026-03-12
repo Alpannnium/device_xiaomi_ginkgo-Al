cab720eba2b3898d04ea5e01dbefab1a5b8d04d5
AlpannVerse <revanzakindar2008@gmail.com>
2026-03-12 10:53:05 +0700

    Update lineage_ginkgo.mk

--- lineage_ginkgo.mk
+++ lineage_ginkgo.mk
@@ -14,6 +14,16 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from ginkgo device
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
+# Enable UI enhancements
+TARGET_ENABLE_BLUR := false
+PERF_ANIM_OVERRIDE := true
+
+# Enable features
+TARGET_SUPPORTS_QUICK_TAP := true
+BYPASS_CHARGE_SUPPORTED := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+USE_PIXEL_CHARGING := true
+
 PRODUCT_NAME := lineage_ginkgo
 PRODUCT_DEVICE := ginkgo
 PRODUCT_MANUFACTURER := Xiaomi
@@ -23,4 +33,4 @@ PRODUCT_MODEL := Redmi Note 8
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Keys
-$(call inherit-product, vendor/private/keys/keys.mk)
+$(call inherit-product, vendor/lineage-priv/keys/keys.mk)
