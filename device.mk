2490fa8a56f4fd030e1c9a33233450259533675d
AlpannVerse <revanzakindar2008@gmail.com>
2026-03-12 10:54:16 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -356,3 +356,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+-include vendor/lineage-priv/keys/keys.mk
